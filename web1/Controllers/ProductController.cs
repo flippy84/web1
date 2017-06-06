fc162d4b345d5ea6c8d64e5a7c29e06afe01561a
flippy84 <ante84@gmail.com>
2017-06-06 18:14:57 +0200

    Added a product page draft

---
+++ web1/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web1.Models;
+
+namespace web1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            BookshopDatabase d = new BookshopDatabase();
+            var products = from p in d.Products select p;
+
+            return View(products.ToList());
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IQueryable<T> Page<T>(this IQueryable<T> query, int page)
+        {
+            return query.Skip((page - 1) * 10).Take(10);
+        }
+    }
+}
