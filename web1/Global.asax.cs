551e5aa7f7ed911d4cf090c491c7b3bfd0b16906
flippy84 <ante84@gmail.com>
2017-06-28 14:37:25 +0200

    Added bootstrap

--- web1/Global.asax.cs
+++ web1/Global.asax.cs
@@ -15,6 +15,7 @@ namespace web1
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
