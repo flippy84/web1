d2370a6f30a218d0df620d9d197d6083b0f86c9d
flippy84 <ante84@gmail.com>
2017-06-29 14:14:44 +0200

    Inline cart and layout fixes

--- web1/Controllers/CartController.cs
+++ web1/Controllers/CartController.cs
@@ -11,7 +11,11 @@ namespace web1.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var cartId = CreateOrGetCartID();
+            BookshopDatabase db = new BookshopDatabase();
+            var rows = from r in db.Carts join product in db.Products on r.ProductId equals product.ProductId where r.CartId == cartId select Tuple.Create(product, r);
+            
+            return View(rows.ToList() ?? new List<Tuple<Product, Cart>>());
         }
 
         public ActionResult Add(int? id)
@@ -40,14 +44,39 @@ namespace web1.Controllers
                 return RedirectToAction("Index", "Product");
         }
 
+        public ActionResult Remove(int? id)
+        {
+            if (id != null)
+            {
+                BookshopDatabase db = new BookshopDatabase();
+                var cartId = CreateOrGetCartID();
+                var row = from r in db.Carts where r.CartId == cartId && r.ProductId == id select r;
+
+                if (row.Any())
+                {
+                    var product = row.First();
+                    product.Amount -= 1;
+                    if (product.Amount == 0)
+                    {
+                        db.Carts.DeleteOnSubmit(product);
+                    }
+                }
+                else
+                {
+                    db.Carts.InsertOnSubmit(new Cart { CartId = cartId, ProductId = id.Value, Amount = 1 });
+                }
+                db.SubmitChanges();
+            }
+
+            if (Request.UrlReferrer != null)
+                return Redirect(Request.UrlReferrer.ToString());
+            else
+                return RedirectToAction("Index", "Product");
+        }
+
         public ActionResult Inline()
         {
-            Guid id = CreateOrGetCartID();
-            using (BookshopDatabase db = new BookshopDatabase())
-            {
-                var items = from item in db.Carts join product in db.Products on item.ProductId equals product.ProductId where item.CartId == id select product;
-            }
-            return View();
+            return Index();
         }
 
         private Guid CreateOrGetCartID()
