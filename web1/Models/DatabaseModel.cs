6b5372d16c103e239e11851b844847957b1047f2
flippy84 <ante84@gmail.com>
2017-07-07 19:46:09 +0200

    Moved database code

---
+++ web1/Models/DatabaseModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace web1.Models
+{
+    [Database]
+    public class BookshopDatabase : DataContext
+    {
+        public BookshopDatabase() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True") { }
+
+        public Table<Product> Products;
+        public Table<Order> Orders;
+        public Table<Cart> Carts;
+        public Table<OrderRow> OrderRows;
+    }
+
+    [Table]
+    public class Product
+    {
+        [Column(IsPrimaryKey = true)] public int ProductId;
+        [Column] public string Name;
+        [Column] public string Description;
+        [Column] public decimal PurchasePrice;
+        [Column] public int Stock;
+    }
+
+    [Table]
+    public class Order
+    {
+        public Order(OrderDetailModel details)
+        {
+            Firstname = details.Firstname;
+            Lastname = details.Lastname;
+            PostAddress = details.PostAddress;
+            PostCode = details.PostCode;
+            PostTown = details.PostTown;
+            Email = details.Email;
+            PhoneNumber = details.PhoneNumber;
+        }
+
+        public Order() { }
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)] public int OrderId;
+        [Column] public string Firstname;
+        [Column] public string Lastname;
+        [Column] public string PostAddress;
+        [Column] public string PostCode;
+        [Column] public string PostTown;
+        [Column] public string Email;
+        [Column] public string PhoneNumber;
+    }
+
+    [Table]
+    public class OrderRow
+    {
+        public OrderRow(int orderId, int productId, int quantity)
+        {
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+        }
+
+        public OrderRow() { }
+
+        [Column(IsPrimaryKey = true)] public int OrderId;
+        [Column(IsPrimaryKey = true)] public int ProductId;
+        [Column] public int Quantity;
+    }
+
+    [Table]
+    public class Cart
+    {
+        [Column(IsPrimaryKey = true)] public Guid CartId;
+        [Column(IsPrimaryKey = true)] public int ProductId;
+        [Column] public int Amount;
+    }
+}
